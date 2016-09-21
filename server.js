97fc714bceb3103db1fa34fd8cc8e7d5a42c9d34
haripriyaaithal <haripriyaaithal@gmail.com>
2016-09-21 20:19:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function(req,res){
+    res.send('This is artical one');
+});
+
+app.get('/artical-two', function(req,res){
+    res.send('This is artical two');
+});
+
+app.get('/artical-three', function(req,res){
+    res.send('This is artical three');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
